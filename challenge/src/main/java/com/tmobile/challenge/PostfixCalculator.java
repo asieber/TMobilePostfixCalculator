6a9112306481d01aecf5af3172c445497881eb5a
00587254 <alexander.sieber@aa.com>
2020-07-01 11:21:51 -0500

    Adding convert postfix to infix

--- challenge/src/main/java/com/tmobile/challenge/PostfixCalculator.java
+++ challenge/src/main/java/com/tmobile/challenge/PostfixCalculator.java
@@ -8,7 +8,7 @@ public class PostfixCalculator {
 		return "+".equals(term) || "-".equals(term) || "*".equals(term) || "/".equals(term);
 	}
 	
-	public static double evaluateSum(String expression) {
+	public static double evaluateExpression(String expression) {
 		Stack<Double> operands = new Stack<Double>();
 		String[] terms = expression.split(" ");
 		for (String term : terms) {
@@ -34,6 +34,38 @@ public class PostfixCalculator {
 			}
 		}
 		return operands.pop();
+	}
+	
+	public static String convertPostfixToInfix(String expression) {
+		Stack<String> expressions = new Stack<String>();
+		String[] terms = expression.split(" ");
+		for (String term : terms) {
+			if (isOperator(term)) {
+				//do operator stuff
+				if ("+".equals(term)) {
+					String second = expressions.pop();
+					expressions.push("(" + expressions.pop() + " + " + second + ")");
+				}
+				if ("-".equals(term)) {
+					String second = expressions.pop();
+					expressions.push("(" + expressions.pop() + " - " + second + ")");
+				}
+				if ("*".equals(term)) {
+					String second = expressions.pop();
+					expressions.push("(" + expressions.pop() + " * " + second + ")");
+				}
+				if ("/".equals(term)) {
+					String denominator = expressions.pop();
+					expressions.push("(" + expressions.pop() + " / " + denominator + ")");
+				}
+			}
+			else {
+				//do operand stuff
+				expressions.push(term);
+			}
+		}
+		String result = expressions.pop();
+		return result.substring(1, result.length()-1);
 	}
 
 }
--- challenge/src/test/java/com/tmobile/challenge/PostfixCalculatorTest.java
+++ challenge/src/test/java/com/tmobile/challenge/PostfixCalculatorTest.java
@@ -8,37 +8,47 @@ public class PostfixCalculatorTest {
 	
 	@Test
 	public void testEvaulateSumInteger() {
-		assertEquals(3.0, PostfixCalculator.evaluateSum("1 2 +"), 0.001);
+		assertEquals(3.0, PostfixCalculator.evaluateExpression("1 2 +"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateSumDouble() {
-		assertEquals(9.75, PostfixCalculator.evaluateSum("4.5 5.25 +"), 0.001);
+		assertEquals(9.75, PostfixCalculator.evaluateExpression("4.5 5.25 +"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateDifferenceInteger() {
-		assertEquals(-1.0, PostfixCalculator.evaluateSum("1 2 -"), 0.001);
+		assertEquals(-1.0, PostfixCalculator.evaluateExpression("1 2 -"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateDifferenceDouble() {
-		assertEquals(-0.75, PostfixCalculator.evaluateSum("4.5 5.25 -"), 0.001);
+		assertEquals(-0.75, PostfixCalculator.evaluateExpression("4.5 5.25 -"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateProductInteger() {
-		assertEquals(2.0, PostfixCalculator.evaluateSum("1 2 *"), 0.001);
+		assertEquals(2.0, PostfixCalculator.evaluateExpression("1 2 *"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateProductDouble() {
-		assertEquals(52, PostfixCalculator.evaluateSum("8 6.5 *"), 0.001);
+		assertEquals(52, PostfixCalculator.evaluateExpression("8 6.5 *"), 0.001);
 	}
 	
 	@Test
 	public void testEvaulateQuotientDouble() {
-		assertEquals(25, PostfixCalculator.evaluateSum("50 2 /"), 0.001);
+		assertEquals(25, PostfixCalculator.evaluateExpression("50 2 /"), 0.001);
+	}
+	
+	@Test
+	public void testEvaulateManyOperandsAndOperators() {
+		assertEquals(-11.5, PostfixCalculator.evaluateExpression("3.5 2 + 4.25 4 * -"), 0.001);
+	}
+	
+	@Test
+	public void testConvertPostfixToInfix() {
+		assertEquals("(3.5 + 2) - (4.25 * 4)", PostfixCalculator.convertPostfixToInfix("3.5 2 + 4.25 4 * -"));
 	}
 
 }
